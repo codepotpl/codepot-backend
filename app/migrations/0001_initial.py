537b0feee6925e4aae848f19a0dbe9d6fb436c35
Maciek Opala <maciek.opala@gmail.com>
2015-04-21 20:51:57 +0200

    Squashed migrations

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.core.validators
 from django.conf import settings
 
 
@@ -15,9 +16,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='UserProfile',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('first_name', models.CharField(max_length=128)),
                 ('last_name', models.CharField(max_length=128)),
+                ('email', models.EmailField(unique=True, max_length=254, validators=[django.core.validators.EmailValidator])),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
         ),
