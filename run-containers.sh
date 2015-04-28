07588ee92bd196b3d372988a7ea9564b93b86448
Maciek Opala <maciek.opala@gmail.com>
2015-04-28 15:20:02 +0200

    Celery confiuration with sample task added.

--- run-containers.sh
+++ run-containers.sh
@@ -15,14 +15,25 @@ docker run \
     -d \
     redis:3.0.0
 
-docker rm -f codepot-backend-development
+docker rm -f codepot-backend-development-celery
 docker run \
     -d \
     --link codepot-redis:redis \
+    -v `pwd`:/app \
+    -e C_FORCE_ROOT="true" \
+    --name codepot-backend-development-celery \
+    codepot-backend-development \
+    celery -A main.celery.app worker -B -E --loglevel=DEBUG
+
+docker rm -f codepot-backend-development
+docker run \
+    -d \
     --link codepot-postgres:postgres \
+    --link codepot-redis:redis \
     -p 8080:8080 \
     -p 2222:22 \
     -v `pwd`:/app \
     -v `pwd`/.pycharm_helpers/:/root/.pycharm_helpers \
     --name codepot-backend-development \
     codepot-backend-development
+
