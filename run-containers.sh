45891bfe19ae3fa33b6b9cef6d85764a0db0d688
Maciek Opala <maciek.opala@gmail.com>
2015-04-28 14:31:38 +0200

    Added redis container.

--- run-containers.sh
+++ run-containers.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
 docker rm -f codepot-postgres
-docker run --name codepot-postgres -e POSTGRES_PASSWORD=lolpass -d --volumes-from codepot-postgres-data postgres:9.4.1
+docker run \
+    --name codepot-postgres \
+    -e POSTGRES_PASSWORD=lolpass \
+    -d \
+    --volumes-from codepot-postgres-data \
+    postgres:9.4.1
+
+docker rm -f codepot-redis
+docker run \
+    --name codepot-redis \
+    -v `pwd`/redis-data:/data \
+    -d \
+    redis:3.0.0
 
 docker rm -f codepot-backend-development
-docker run -d --link codepot-postgres:postgres -p 8080:8080 -p 2222:22 -v `pwd`:/app -v `pwd`/.pycharm_helpers/:/root/.pycharm_helpers --name codepot-backend-development codepot-backend-development
+docker run \
+    -d \
+    --link codepot-redis:redis \
+    --link codepot-postgres:postgres \
+    -p 8080:8080 \
+    -p 2222:22 \
+    -v `pwd`:/app \
+    -v `pwd`/.pycharm_helpers/:/root/.pycharm_helpers \
+    --name codepot-backend-development \
+    codepot-backend-development
