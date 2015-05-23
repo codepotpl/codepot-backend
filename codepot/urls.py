cfa9a67621d3af5d22f0b8d3c5f15d4e2114d2dd
Maciek Opala <maciek.opala@gmail.com>
2015-05-23 17:45:56 +0200

    Added purchases endpoint.

--- codepot/urls.py
+++ codepot/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import (
     url,
 )
 
-from codepot.views import auth as auth_views
-
+from codepot.views import (
+    auth as auth_views,
+    purchases as purchases_views,
+)
 
 auth_patterns = patterns(
     '',
@@ -12,4 +14,9 @@ auth_patterns = patterns(
     url(r'^auth/sign-up/$', auth_views.sign_up),
 )
 
-urlpatterns = auth_patterns
+purchase_patterns = patterns(
+    '',
+    url(r'^purchases/$', purchases_views.handle_purchase)
+)
+
+urlpatterns = auth_patterns + purchase_patterns
---
+++ codepot/views/purchases/__init__.py
@@ -0,0 +1,17 @@
+from django.db import transaction
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+)
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
+
+
+@api_view(['POST', ])
+@permission_classes((IsAuthenticated,))
+# @parser_classes((parser_class_for_schema(sign_up_req_schema),))
+@transaction.atomic()
+def handle_purchase(request, **kwargs):
+    return Response(status=HTTP_204_NO_CONTENT)
