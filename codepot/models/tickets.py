85f181cce9a3f14b6826ac61588c97b5073223bb
Maciek Opala <maciek.opala@gmail.com>
2015-05-25 21:32:48 +0200

    Purchase - ticket relation.

--- codepot/models/tickets.py
+++ codepot/models/tickets.py
@@ -16,7 +16,7 @@ class TicketTypeName(Enum):
 
 class Ticket(models.Model):
     ticket_id = models.CharField(primary_key=True, max_length=32, default=create_hash)
-    purchase = models.ForeignKey('codepot.Purchase')
+    purchase = models.OneToOneField('codepot.Purchase')
     created = models.DateTimeField(default=datetime.datetime.now, null=False, blank=False)
     type = models.CharField(max_length=64, choices=enum_to_model_choices(TicketTypeName), null=False, blank=False)
 
