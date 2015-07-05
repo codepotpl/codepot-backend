d91f51c9c4c4673453c9f512c62921c35ec3beb8
Maciek Opala <maciek.opala@gmail.com>
2015-07-05 20:42:21 +0200

    Prices made public.

--- codepot/views/prices/__init__.py
+++ codepot/views/prices/__init__.py
@@ -1,11 +1,7 @@
 import time
 
 from django.utils import timezone
-from rest_framework.decorators import (
-    api_view,
-    permission_classes,
-)
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 
@@ -13,7 +9,6 @@ from codepot.models import Product
 
 
 @api_view(['GET', ])
-@permission_classes((IsAuthenticated,))
 def get_prices(request, **kwargs):
     products = Product.objects.all()
     now = timezone.now()
