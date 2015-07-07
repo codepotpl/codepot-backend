d843b9c51888fb0391fb303f1aa3d74e00fcd0c1
Maciek Opala <maciek.opala@gmail.com>
2015-07-07 10:43:00 +0200

    Allow to access prices without auth.

--- codepot/views/prices/__init__.py
+++ codepot/views/prices/__init__.py
@@ -1,14 +1,18 @@
 import time
 
 from django.utils import timezone
-from rest_framework.decorators import api_view
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+)
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 
 from codepot.models import Product
 
-
 @api_view(['GET', ])
+@permission_classes((AllowAny,))
 def get_prices(request, **kwargs):
     products = Product.objects.all()
     now = timezone.now()
