d0db090c68550cd983b1c0843c0865b73925ed5e
Maciek Opala <maciek.opala@gmail.com>
2015-07-20 16:44:16 +0200

    Added 'GROUP' purchase type.

--- codepot/views/purchases/__init__.py
+++ codepot/views/purchases/__init__.py
@@ -42,7 +42,6 @@ from codepot.views.purchases.exceptions import (
     InvalidPaymentInfoException,
 )
 
-
 @api_view(['POST', ])
 @permission_classes((IsAuthenticated,))
 @parser_classes((parser_class_for_schema(purchases_json_schema.make_purchase_req_schema),))
@@ -114,11 +113,9 @@ def handle_new_purchase(request, **kwargs):
 
     return Response(build_purchase_response(purchase), HTTP_201_CREATED)
 
-
 def _check_if_registration_open():
     if not AppSettings.objects.is_registration_open():
         raise RegistrationClosedException()
-
 
 def _check_if_tickets_limit_exceeded():
     all_purchases = _count_success_purchases()
@@ -139,26 +136,20 @@ def _check_if_tickets_limit_exceeded():
     if (all_purchases - sum_excluded) >= purchases_limit:
         raise TicketsLimitExceededException()
 
-
 def _count_success_purchases():
     return Purchase.objects.filter(payment_status=PaymentStatusName.SUCCESS.value).count()
-
 
 def _count_organizers_purchases():
     return __count_purchases_for_promo_code_classification('organizers')
 
-
 def _count_volunteers_purchases():
     return __count_purchases_for_promo_code_classification('volunteers')
-
 
 def _count_speakers_purchases():
     return __count_purchases_for_promo_code_classification('speakers')
 
-
 def _count_sponsors_staff_purchases():
     return __count_purchases_for_promo_code_classification('sponsors staff')
-
 
 def __count_purchases_for_promo_code_classification(classification):
     return Purchase.objects.filter(promo_code__classification__name__istartswith=classification).count()
@@ -170,7 +161,6 @@ def _check_if_user_has_purchase(user):
         raise UserAlreadyHasPurchaseException(user.id, p.id)
     except Purchase.DoesNotExist:
         pass
-
 
 def _find_and_validate_product(product_id):
     product = _find_product_or_raise(product_id)
@@ -192,7 +182,6 @@ def _check_if_product_is_active(product):
     if not (date_from < now < date_to):
         logger.error('Product for ID: {} is not active, from: {}, to: {}'.format(product.id, date_from, date_to))
         raise ProductInactiveException(product.id)
-
 
 def _validate_payment_info(payment_type, payment_info):
     if payment_type == PaymentTypeName.PAYU.value:
@@ -226,7 +215,6 @@ def _check_if_promo_code_has_valid_usage_limit(promo_code):
         logger.error('Promo code: {} has exceeded usage limit'.format(promo_code.code))
         raise PromoCodeForPurchaseHasExceededUsageLimit(promo_code.code)
 
-
 def _set_invoice_data(purchase, invoice):
     purchase_invoice = PurchaseInvoice()
     purchase_invoice.name = invoice['name']
@@ -250,7 +238,6 @@ def _calculate_price(user, product, discount):
 
     return (price_net, price_total)
 
-
 def _handle_payu_payment(user, ip_address, price_total, purchase, redirect_link):
     buyer = Buyer(user.first_name, user.last_name, user.email, ip_address)
     payu_product = PayUProduct(purchase.product.name, price_total, 1)
@@ -263,7 +250,6 @@ def _handle_payu_payment(user, ip_address, price_total, purchase, redirect_link)
     )
     purchase.payu_payment = PayuPayment.objects.get(payment_id=payment_id)
     purchase.payu_payment_link = follow
-
 
 def build_purchase_response(purchase):
     return {
@@ -281,7 +267,6 @@ def build_purchase_response(purchase):
         }
     }
 
-
 def _get_purchase_invoice_data_or_none(purchase):
     try:
         invoice = PurchaseInvoice.objects.get(purchase=purchase)
@@ -297,7 +282,6 @@ def _get_purchase_invoice_data_or_none(purchase):
         logger.info('Skipping invoice processing, no invoice for purchase: {}, err: {}'.format(purchase.id, str(e)))
         return None
 
-
 def _prepare_payment_info(purchase):
     if purchase.payment_type == PaymentTypeName.FREE.value:
         return None
@@ -310,7 +294,6 @@ def _prepare_payment_info(purchase):
     else:
         raise Exception(
             'Invalid purchase payment type. Purchase: {}, payment type: {}'.format(purchase.id, purchase.payment_type))
-
 
 def _prepare_transfer_payment_info(purchase):
     ret = {}
